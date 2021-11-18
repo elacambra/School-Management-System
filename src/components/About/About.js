48f61c6ea7f6295f3659cf91203f510d6216c094
Enrique Lacambra III <enriquelacambraiii@gmail.com>
2021-11-18 16:22:49 +0800

    updated working

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './About.scss';
+
 function About() {
 	return (
 		<div id='About'>
@@ -7,21 +8,10 @@ function About() {
 			<div className='row'>
 				<div className='col-md-6'>
 					<p>
-						This is an open source app which is built for showing some programming skills purpose, for sure
-						i could`ve created a system that enables any username and password to login but there`s no need
-						for this
-					</p>
-					<p>
-						This app has no authentication (signup/login) system which means that any one can start using,
-						adding into, removing from, and editing to this system{' '}
-					</p>
-				</div>
-				<div className='col-md-6'>
-					<p>
 						This is a school management system in which you can manage the school subjects, teachers,
-						students and classes
+						students and classes. 
 					</p>
-					<p>Start using the app, navigate from the navbar at the right side, happy hacking :)</p>
+					<p>Start using the app, navigate from the navbar at the right side :)</p>
 				</div>
 			</div>
 		</div>
